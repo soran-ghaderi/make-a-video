87aa2f3268d76eddd01e9f289be83af6d077136d
Soran Ghaderi <soran.gdr.cs@gmail.com>
2022-10-06 03:45:00 +0330

    doc: conv_T

--- make_a_video/components/layers/p3d_attention.py
+++ make_a_video/components/layers/p3d_attention.py
@@ -34,16 +34,44 @@ def conv_S(output_channels: int, stride: Optional[Tuple[int, Tuple]] = 1,
                                   use_bias=False)
 
 
-def conv_T(output_channels, stride=1, padding=1):
+def conv_T(output_channels, stride=1, padding='valid'):
+    """Applies a 3D convolution over an input signal composed of several input planes.
+
+    A (1, 3, 3) convolution layer as described in [1]_
+
+    References
+    ----------
+    [1] https://openaccess.thecvf.com/content_ICCV_2017/papers/Qiu_Learning_Spatio-Temporal_Representation_ICCV_2017_paper.pdf
+
+    Parameters
+    ----------
+    output_channels :
+        Number of channels produced by the convolution
+    stride :
+        Stride of the convolution. Default: 1
+    padding :
+        Padding added to all six sides of the input
+
+    Returns
+    -------
+    out:
+        A tensor of rank 5+ representing
+    """
 
     return tf.keras.layers.Conv3D(output_channels, kernel_size=[3, 1, 1], strides=stride, padding=padding,
                                   use_bias=False)
 
 
 if __name__ == '__main__':
-    m = conv_S(16, padding=(4, 2, 0))
-    input = tf.random.normal((20, 7, 3, 10, 50, 100))
+    m = conv_S(16)
+    input = tf.random.normal((20, 7, 20, 10, 50, 3))
     print(input.shape)
     print("Shape of the", m(input).shape)
+
+    ct = conv_T(16)
+    print(input.shape)
+    print("Shape of the", ct(input).shape)
+
+
 
 
